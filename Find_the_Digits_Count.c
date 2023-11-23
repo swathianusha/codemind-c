c8c6f0e77c4df02911ba0531c5679644293c4591
Swathi Anusha .Ummidi <147793429+swathianusha@users.noreply.github.com>
2023-11-23 10:00:00 +0530

    #include<stdio.h> int main() {     int i,n,r,dc=0;     scanf("%d",&n);     i=1;     while(n!=0)     {         r=n%10;         dc=dc+1;         n=n/10;
        }
        printf("%d",dc);
    }

--- Find_the_Digits_Count.c
+++ Find_the_Digits_Count.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,r,N,dc=0;
-    scanf("%d",&N);
-    while(N!=0)
+    int i,n,r,dc=0;
+    scanf("%d",&n);
+    i=1;
+    while(n!=0)
     {
-        r=N%10;
-        N=N/10;
+        r=n%10;
         dc=dc+1;
+        n=n/10;
+        
     }
     printf("%d",dc);
-    
 }
