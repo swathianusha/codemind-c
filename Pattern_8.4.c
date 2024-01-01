ffd502d9edfdcdff7286b14b44ce13457c239fe7
Swathi Anusha .Ummidi <147793429+swathianusha@users.noreply.github.com>
2024-01-01 10:09:50 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j || j==(n-i+1))             {                 printf("%c",i+64);             }             else             {                 printf(" ");             }         }         printf(" ");
        }
            
    }

---
+++ Pattern_8.4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==j || j==(n-i+1))
+            {
+                printf("%c",i+64);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+        
+    }
+        
+}
