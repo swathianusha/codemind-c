0351bb9f904e451aa8a2323a0700790cff712a2d
Swathi Anusha .Ummidi <147793429+swathianusha@users.noreply.github.com>
2024-01-03 22:59:10 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int count=0;     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             count++;         }     }     if(count==n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Even_Array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int count=0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            count++;
+        }
+    }
+    if(count==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
